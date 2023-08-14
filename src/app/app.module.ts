b28f6f17fa214021673f92ab0e0c6eb51da08849
Kfir Azulay <kfira@moveohls.com>
2023-08-14 16:23:24 +0300

    Files setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NewsModule } from '../news/news.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+      ConfigModule.forRoot({
+          isGlobal: true,
+      }),
+      MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+              uri: configService.get<string>('DB_URL'),
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+          }),
+          inject: [ConfigService],
+      }),
+    NewsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
